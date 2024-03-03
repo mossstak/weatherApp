a25ea57f2eaf7be33d259d92eeb21c671a3c158d
Mostak Khan <mostak1993@gmail.com>
2024-03-03 07:54:36 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen w-screen grid grid-cols-1 place-self-center content-center bg-gray-200">
+      <h1 className="text-[3em] text-center">Hello World</h1>
+      <p className="text-center font-bold text-xl">Vite + ReactJS + tailwindcss</p>
+    </div>
+  )
+}
+
+export default App
